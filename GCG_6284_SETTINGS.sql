84219a9d9e8abee4a363480c5af75b046eca948f
Service Web <sw@gssmail.com>
2024-03-18 13:40:40 -0500

    Commit changes in C:\Websites\Git\emarlow\6284\GCG_6284_SETTINGS.sql

---
+++ GCG_6284_SETTINGS.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_6284_SETTINGS" (
+ "REC_ID" INTEGER,
+ "RemoteHost" CHAR(50),
+ "RemoteUser" CHAR(50),
+ "RemotePassword" CHAR(50),
+ "RemotePort" CHAR(50),
+ "RemotePath" CHAR(50),
+ "DATE_LAST_CHG" CHAR(8),
+ "TIME_LAST_CHG" CHAR(8),
+ "PID" INTEGER,
+ "DEL_FILE" CHAR (1) );
